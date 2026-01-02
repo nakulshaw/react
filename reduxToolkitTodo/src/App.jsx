3318f83ac32da0f132d5953bee2a8d063647cf38
Nakul Shaw <nakulkelvin@gmail.com>
2026-01-02 13:08:22 +0530

    reduxToolkitTodo

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import {AddTodo} from "./components/AddTodo.jsx"
+import Todos from "./components/Todos.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
